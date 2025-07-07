6b7f804ee8c420de9792b00c484c41a64064b9f0
Benjamin Gluck <benjamin.gluck@renaissancetech.com>
2025-07-07 14:45:03 -0400

    add simple variant printer

--- src/main.py
+++ src/main.py
@@ -1,6 +1,8 @@
+import orjson
 from producer import GenerateVariants
 from consumer import Sender
 import sqlite3
+from pprint import pprint
 
 # Meant for local testing
 def create_tables(db_path):
@@ -25,10 +27,20 @@ def create_tables(db_path):
     conn.commit()
     conn.close()
     
+def print_rows():
+    conn = sqlite3.connect("variants.db")
+    cursor = conn.cursor()
+    cursor.execute("SELECT * FROM queue LIMIT 2")
+    for row in cursor.fetchall():
+        pprint(orjson.loads(row[1]))
+    conn.close()
+
 def main():
     create_tables("variants.db")
     producer = GenerateVariants("reference/output.json", "variants.db", seed=42)
     producer.generate()
+
+    print_rows()
 
     sender = Sender("variants.db", "https://httpbin.org/post")
     sender.run(num_workers=50)
